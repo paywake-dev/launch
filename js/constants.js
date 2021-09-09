5dec99542863b2a3b4b514516b5844d3ca6d9ee3
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-09 14:32:44 -0700

    functionless site

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,7 @@
 const YEAR = (new Date()).getFullYear()
+
+const ESTIMATED_RETURN = 10
+const SLIDER_INIT_MIN = 30
+const SLIDER_INIT_MAX = 80
+const LAUNCH_DATE = new Date("Oct 27, 2021 5:00:00").getTime();
+const COUNTDOWN_PAD = 1
