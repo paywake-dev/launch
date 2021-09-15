653096b91b6434df1b0c8462be3430814f4abb88
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-14 20:26:58 -0700

    increasing slider min

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ const YEAR = (new Date()).getFullYear()
 
 const COLLECT_API = "https://ug3on9zs62.execute-api.us-east-1.amazonaws.com/dev/index/collect"
 const ESTIMATED_RETURN = 10
-const SLIDER_INIT_MIN = 30
+const SLIDER_INIT_MIN = 40
 const SLIDER_INIT_MAX = 80
 const SLIDER_DURATION_MS = 1600
 const LAUNCH_DATE = new Date("Oct 27, 2021 5:00:00").getTime()
