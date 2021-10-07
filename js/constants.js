24d9790dd451952287afcdec82c49ee69f305ec6
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-10-06 17:30:07 -0700

    pushing back launch to nov 3

--- js/constants.js
+++ js/constants.js
@@ -5,5 +5,5 @@ const ESTIMATED_RETURN = 10
 const SLIDER_INIT_MIN = 40
 const SLIDER_INIT_MAX = 80
 const SLIDER_DURATION_MS = 1600
-const LAUNCH_DATE = new Date("Oct 27, 2021 5:00:00").getTime()
+const LAUNCH_DATE = new Date("Nov 3, 2021 5:00:00").getTime()
 const COUNTDOWN_PAD = 1
