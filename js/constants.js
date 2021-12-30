16331494b1c7af49afefd78cdcf2bb95a99a8dad
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-30 11:14:15 -1100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const YEAR = (new Date()).getFullYear()
 
 const COLLECT_API = "https://ug3on9zs62.execute-api.us-east-1.amazonaws.com/dev/index/collect"
-const ESTIMATED_RETURN = 10
+const ESTIMATED_RETURN = 9
 const SLIDER_INIT_MIN = 40
 const SLIDER_INIT_MAX = 80
 const SLIDER_DURATION_MS = 1600
