adf86f38ff7cbe06de112eeb73bd536634570393
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-30 11:08:53 -1100

    pushing launch to February 9th

--- js/constants.js
+++ js/constants.js
@@ -5,5 +5,5 @@ const ESTIMATED_RETURN = 10
 const SLIDER_INIT_MIN = 40
 const SLIDER_INIT_MAX = 80
 const SLIDER_DURATION_MS = 1600
-const LAUNCH_DATE = new Date("Jan 26, 2022 5:00:00").getTime()
+const LAUNCH_DATE = new Date("Feb 9, 2022 5:00:00").getTime()
 const COUNTDOWN_PAD = 1
